2857802b07cbd1f7dc502c2841a8ec7214980530
Andrew Mahon (Type/Code) <andrew@typeslashcode.com>
2011-08-07 16:18:03 -0400

    some server stuff

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import sys
 import os
 import datetime
 import hashlib
+import time
+from datetime import datetime
+import json
 
 sys.path.append("lib")
 import tornado.httpserver
@@ -13,6 +16,7 @@ import tornado.web
 
 import pymongo
 from pymongo import Connection
+from pymongo import json_util
 
 connection = Connection('localhost', 27017)
 db = connection.its_almost
@@ -20,17 +24,26 @@ timers = db['timers']
 
 class ItsAlmost(tornado.web.RequestHandler):
   def get(self,id):
-    timer = timers.find_one({"id": id})
+    timer = timers.find_one({"id": id,'expires':{'$gte':datetime.now()}})
     if timer is not None:
-      return self.write(tornado.escape.json_encode(timer));
-    raise tornado.web.HTTPError(404)
+      timer[u'expires'] = (time.mktime(timer[u'expires'].timetuple()) * 1000)
+      return self.write(json.dumps([timer],default=json_util.default));
+    return self.write(json.dumps([],default=json_util.default));
     
-  def post(self):
-    print(self)
-    self.write("Thanx")
+  def post(self,id):
+    timer_id = timers.insert({
+      'id':id,
+      'name':self.get_argument('name'),
+      'expires':datetime.fromtimestamp(float(self.get_argument('expires'))/1000)
+    });
+    
+    timer = timers.find_one({"_id": timer_id})
+    if timer is not None:
+      timer[u'expires'] = (time.mktime(timer[u'expires'].timetuple()) * 1000)
+      return self.write(json.dumps([timer],default=json_util.default));
+    return self.write(json.dumps([],default=json_util.default));
 
 application = tornado.web.Application([
-  (r"/timer", ItsAlmost),
   (r"/timer/(.*)", ItsAlmost)
 ])
 
